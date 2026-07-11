1fae09b7c17cbc0b243b867eb6410362ed09420a
Lu SpaceAI <irsyadtawakal@gmail.com>
2026-07-11 14:06:35 +0700

    fix: explicit file extensions for Turbopack module resolution

--- packages/design-system/src/index.ts
+++ packages/design-system/src/index.ts
@@ -1,10 +1,10 @@
-export * from './tokens'
-export * from './components'
-export { cn } from './utils/cn'
+export * from './tokens/index.ts'
+export * from './components/index.ts'
+export { cn } from './utils/cn.ts'
 export {
   isRunningInWebView,
   isWebViewUserAgent,
   postToNative,
-} from './utils/webview'
-export type { NativeBridgeMessage } from './utils/webview'
-export type * from './types'
+} from './utils/webview.ts'
+export type { NativeBridgeMessage } from './utils/webview.ts'
+export type * from './types.ts'
